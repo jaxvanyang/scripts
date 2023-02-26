0dcf236bb25d679c4551235171f5f256e535bf09
Jax Young <jaxvanyang@gmail.com>
2023-02-26 12:15:21 +0800

    docs(subtitles-rename): add description comment

--- autorename/subtitles-rename.sh
+++ autorename/subtitles-rename.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
+#
+# Rename indexed subtitles with corresponding eposides.
+# Subtitle files and video files must in the same directory.
+#
+# Usage:
+# 	subtitles-rename [season] [eposides] [postfix]
+#
+# Naming example:
+# 	"${S01E01_name}.mkv" + "${foo}S01E01${bar}.ass" -> "${S01E01_name}.${postfix}.ass"
+# 	...
+# 	"${S01E10_name}.mkv" + "${foo}S01E10${bar}.ass" -> "${S01E10_name}.${postfix}.ass"
+#
+# References:
+# - https://support.emby.media/support/solutions/articles/44001159160-subtitles
 
+set -ex
+
+# Here are the variables for naming, don't forget that
+# their values can be changed using command line arguments.
 season=01
 eposides=10
 postfix=".Chinese(中英双语).default"
