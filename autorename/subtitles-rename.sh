0bcb10a6651bebf79186e4d7dad7f85c4677b766
Jax Young <jaxvanyang@gmail.com>
2023-02-26 12:14:13 +0800

    feat(subtitles-rename): add postfix argument

--- autorename/subtitles-rename.sh
+++ autorename/subtitles-rename.sh
@@ -2,6 +2,7 @@
 
 season=01
 eposides=10
+postfix=".Chinese(中英双语).default"
 
 rename() {
 	for i in $(seq -f "%02g" "$eposides"); do
@@ -9,7 +10,7 @@ rename() {
 		video="$(find . -name "*${id}*.mkv")"
 		sub="$(find . -name "*${id}*.ass")"
 
-		newname="${video//.mkv/.Chinese(中英双语).default.ass}"
+		newname="${video//.mkv/${postfix}.ass}"
 
 		mv "${sub}" "${newname}"
 	done
@@ -23,6 +24,11 @@ main() {
 
 	if [ $# -ne 0 ]; then
 		eposides="${1}"
+		shift
+	fi
+
+	if [ $# -ne 0 ]; then
+		postfix="${1}"
 		shift
 	fi
 
