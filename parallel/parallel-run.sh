2106d2bec3dacfda4eceacb66cc82189675c7360
Jax Young <jaxvanyang@gmail.com>
2023-03-14 12:52:16 +0800

    feat(parallel): add n_process option to parallel-run

--- parallel/parallel-run.sh
+++ parallel/parallel-run.sh
@@ -5,19 +5,25 @@
 # Note: The script file must not contains single quote "'"
 #
 # Usage:
-# 	parallel <script.sh>
+# 	parallel <script.sh> [n_process]
 
 parallel-run() {
-	gawk '{print $0 " &> " NR ".log"}' "${1}" | sed "s/^.*\$/'\0'/" | xargs -L 1 -P 4 sh -c
+	gawk '{print $0 " &> " NR ".log"}' "${1}" | sed "s/^.*\$/'\0'/" | xargs -L 1 -P "${n_process}" sh -c
 }
 
 main() {
-	if [ $# -ne 1 ]; then
-		echo "You must specify one and only one argument!" >&2
+	n_process="$(nproc)"
+
+	if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+		echo "You must specify one or two arguments!" >&2
 		exit 1
 	fi
 
-	parallel-run "${1}"
+	if [ $# -eq 2 ]; then
+		n_process="${2}"
+	fi
+
+	parallel-run "${1}" "${n_process}"
 }
 
 main "$@"
